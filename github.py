d1243e406e84f350a808659c78f7bf99567cd818
Manav-codehub <169026647+Manav-codehub@users.noreply.github.com>
2025-05-26 18:32:16 +0530

    diwakar

--- github.py
+++ github.py
@@ -1,6 +1,4 @@
-# public class HelloWorld {
-#     public static void main(String[] args) {
-#         // Print "Hello, World!" to the console
-#         System.out.println("Hello, World!");
-#     }
-# }
+def square(x):
+    return x*x
+
+print(square(2))
